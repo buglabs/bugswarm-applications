3613035c9ab32ab1540b1d30b5f4acc863f1bc68
Davis Barber <davis.barber@buglabs.net>
2011-10-07 16:49:43 -0400

    debugged swarmlight

--- swarmlight/run.sh
+++ swarmlight/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkfifo slfifo &&
+sleep 1
 ./swarmlight.py &
-./bugswarm-tools/consume.py consume 3f661d513bda60bd41419666dd8fa381ec17e979 > slfifo &
+sleep 1
+nohup ./bugswarm-tools/consume.py consume 3f661d513bda60bd41419666dd8fa381ec17e979 > slfifo &
