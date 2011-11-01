d6ed5671c3e9373cd9f38511292b67566f3b2c44
barberdt <barberdt@gmail.com>
2011-11-01 17:00:24 -0400

    adds option parser to bugstats and removes excess scripts

--- bugstats/bugstats.py
+++ bugstats/bugstats.py
@@ -6,23 +6,52 @@ import subprocess
 import threading
 import sys
 import json
-from datetime import datetime
 import signal
 import socket
 import random
+from datetime import datetime
+from optparse import OptionParser
 
 conn = None
 hostname = "api.bugswarm.net"
 api_key = "7a849e6548dbd6f8034bb7cc1a37caa0b1a2654b"
 swarm_id = "0c966d48e16908975ae483642ed7302e7b6ec7d7"
-resource_id = "39525163ebef8bc83dc6fd891e7cea1e8fe21987"
+resource_map = {"1":"1c85f72ef0a57fc2722540294e349343fccfd1c1", "2":"1f8757afbd3b814f85e1da02fe6ce1c802abb1a7", "3":"6a7166dd0f14cdca9b95f260cbee86129aac8991", "4":"29f7ba8c5fc61cbf3fc97f43d72b755c55073cfd", "5":"9cd5ee810ca0f8ed99114ef5e17afa854e3852ea", "6":"8969c03718274cbb62be0ab056b9d37c6471fe4a", "7":"b1eb3f1c13b581a1f5b962e4d5378f10f06780dd", "8":"39525163ebef8bc83dc6fd891e7cea1e8fe21987", "9":"a540cd12f6e098cdde71404bab964a2302da2f23", "10":"4588344415bc95e0edd00a51e33452ab70808878"}
+resource_id = None
+produce_fake = False
 interval_producer = None
 immediate_producer = None
 
 def main():
+    opt_usage = "usage: \n %s BUG_NUMBER [options]"%(sys.argv[0])
+    opt_usage += "\n\n *BUG_NUMBER: The number of the BUG you are running BUGstats for. Valid numbers; 1-10."
+    parser = OptionParser(usage = opt_usage)
+    parser.add_option("-f", action="store_true", dest="fake", help="Run with fake statistics.", default=False)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        print "Invalid number of args. See --help for correct usage."
+        sys.exit()
+    bug_number = args[0]
+    bugstats(bug_number, options.fake)
+
+def bugstats(bug_number, fake):
+    global resource_id, produce_fake
+    try:
+        if (int(bug_number) > 10) or (int(bug_number < 1)):
+            print "BUG_NUMBER must be between 1 and 10"
+            sys.exit()
+    except ValueError:
+        print "BUG_NUMBER must be an integer between 1 and 10"
+        sys.exit()
+    resource_id = get_resource_id(bug_number)
+    if fake == True:
+        produce_fake = True
     signal.signal(signal.SIGINT, signal_handler)
     swarm_init()
     participate()
+
+def get_resource_id(bug_number):
+    return resource_map[bug_number]
 
 def swarm_init():
     global hostname, api_key, swarm_id, resource_id
@@ -62,8 +91,11 @@ def send_message(msg):
         print 'A problem has occured: ', e
 
 def produce_stats_public(isImmediate):
-    #stats = get_stats()
-    stats = get_fake_stats()
+    global produce_fake
+    if produce_fake == True:
+        stats = get_fake_stats()
+    else:
+        stats = get_stats()
     if isImmediate == True:
         stats["immediate"] = True;
     else:
@@ -79,7 +111,7 @@ def get_stats():
     stats["nice"] = mpstat_out[4]
     stats["sys"] = mpstat_out[5]
     stats["datetime"] = str(datetime.now())
-    stats["position"] = {"lat": 41.4, "lon": -73.3}
+    stats["position"] = {"lat": 40.73, "lon": -73.99}
     return stats
 
 def get_fake_stats():
@@ -88,7 +120,7 @@ def get_fake_stats():
     stats["nice"] = random.uniform(0,10)
     stats["sys"] = random.uniform(0,10)
     stats["datetime"] = str(datetime.now())
-    stats["position"] = {"lat": 41.4, "lon": -73.3}
+    stats["position"] = {"lat": 40.73, "lon": -73.99}
     return stats
 
 def signal_handler(signal, frame):
